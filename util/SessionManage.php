5151d82b50aa3f1f28726100f1da31b280bfda42
FEDERICO GIANGRANDI <federico.giangrandi@haveasync.com>
2013-06-04 11:48:18 +0200

    n'ata vota

--- util/SessionManage.php
+++ util/SessionManage.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,16 +13,15 @@ class SessionManage {
     private $utente;
 
     public function __construct() {
-       if(!isset($_SESSION['user'])){
-		   
+       if($_SESSION != null && !isset($_SESSION['user'])){
            session_start();
        }
     }
     
     public function checkUtente(){
-        if(!isset($_SESSION['user'])){
+        if($_SESSION != null && !isset($_SESSION['user'])){
             session_destroy();
-            header('Location: index.php?page=login/login');
+            header('Location: index.php?page=login/login&lang='.$_REQUEST['lang']);
         }
     }
     
@@ -42,7 +40,5 @@ class SessionManage {
 
 $sessione = new SessionManage();
 $utente = $sessione->getUtente();
-
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-
 ?>
